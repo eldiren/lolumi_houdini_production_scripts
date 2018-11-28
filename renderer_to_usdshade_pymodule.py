ff736b692d115904536b0d8cb9e8f7594327c527
Chau Heart <chau.heart@gmail.com>
2018-11-28 05:22:17 -0500

    Added texture functionality, changed to using USDPreviewSurface VOP for conversion

--- renderer_to_usdshade_pymodule.py
+++ renderer_to_usdshade_pymodule.py
@@ -21,10 +21,11 @@ def set_usd_filename():
     hou.node('./usdrop1').parm('usdfile').set(filename+'.usda')
     
 # should create a new material with passed in properties
-def newMaterial(stage, path, dclr, roughness, metallic, eclr=(0,0,0), opacity=1.0, ior=1.5, clearcoat=0.0, clearroug=0.0):
+def newMaterial(stage, path, dclr, roughness, metallic, eclr=(0,0,0), opacity=1.0, ior=1.5, clearcoat=0.0, clearroug=0.0, occlusion=0.0):
     # TODO: keep adding more support, missing textures for now
     material = UsdShade.Material.Define(stage, path)
     pbrShader = UsdShade.Shader.Define(stage, path + '/PBRShader')
+
     pbrShader.CreateIdAttr("UsdPreviewSurface")
     pbrShader.CreateInput("diffuseColor", Sdf.ValueTypeNames.Color3f).Set(dclr)
     pbrShader.CreateInput("roughness", Sdf.ValueTypeNames.Float).Set(roughness)
@@ -40,22 +41,45 @@ def newMaterial(stage, path, dclr, roughness, metallic, eclr=(0,0,0), opacity=1.
     return
 
 def newMaterialViaShop(shopshader, stage, path):
-    # TODO: keep adding more support, missing textures for now
+    diffSet = False
     material = UsdShade.Material.Define(stage, path)
-    dclr = Gf.Vec3f(shopshader.parmTuple('base_color')[0].eval(), shopshader.parmTuple('base_color')[1].eval(), shopshader.parmTuple('base_color')[2].eval())
-    roughness = shopshader.parm('specular_roughness').eval()
-    metallic = shopshader.parm('metalness').eval()
-    eclr = Gf.Vec3f(0,0,0)
-    if shopshader.parm('emission').eval() > 0:
-        eclr = Gf.Vec3f(shopshader.parmTuple('emission_color')[0].eval(), shopshader.parmTuple('emission_color')[1].eval(), shopshader.parmTuple('emission_color')[2].eval())
-    opacity = shopshader.parmTuple('opacity')[0].eval()
-    ior = shopshader.parm('specular_IOR').eval()
-    clearcoat = shopshader.parm('coat').eval()
-    clearroug = shopshader.parm('coat_roughness').eval()
     
     pbrShader = UsdShade.Shader.Define(stage, path + '/PBRShader')
     pbrShader.CreateIdAttr('UsdPreviewSurface')
-    pbrShader.CreateInput('diffuseColor', Sdf.ValueTypeNames.Color3f).Set(dclr)
+    
+    uvReader = UsdShade.Shader.Define(stage, path + '/uvReader')
+    uvReader.CreateIdAttr('UsdPrimvarReader_float2')
+    uvReader.CreateInput('varname',Sdf.ValueTypeNames.Token).Set('uv')
+    uvReader.CreateOutput('result', Sdf.ValueTypeNames.Float2)
+
+    inputs = shopshader.inputConnections()
+    for nodeInput in inputs:
+        if nodeInput.outputName() == 'diffuseColor':
+            inNode = nodeInput.inputNode()
+            # create texture sampler node
+            textureSampler = UsdShade.Shader.Define(stage, path + '/diffuseTexture')
+            textureSampler.CreateIdAttr('UsdUVTexture')
+            textureSampler.CreateInput('file', Sdf.ValueTypeNames.Asset).Set(inNode.parm('file').eval())
+            textureSampler.CreateInput('uv', Sdf.ValueTypeNames.Float2).ConnectToSource(uvReader, 'result')
+            textureSampler.CreateOutput('rgb', Sdf.ValueTypeNames.Float3)
+        
+            pbrShader.CreateInput('diffuseColor', Sdf.ValueTypeNames.Color3f).ConnectToSource(textureSampler, 'rgb')
+            
+            diffSet = True
+            
+    if not diffSet:
+        dclr = Gf.Vec3f(shopshader.parmTuple('diffuseColor')[0].eval(), shopshader.parmTuple('diffuseColor')[1].eval(), shopshader.parmTuple('diffuseColor')[2].eval())
+        pbrShader.CreateInput('diffuseColor', Sdf.ValueTypeNames.Color3f).Set(dclr)
+        
+    roughness = shopshader.parm('roughness').eval()
+    metallic = shopshader.parm('metallic').eval()
+    eclr = Gf.Vec3f(shopshader.parmTuple('emissiveColor')[0].eval(), shopshader.parmTuple('emissiveColor')[1].eval(), shopshader.parmTuple('emissiveColor')[2].eval())
+    opacity = shopshader.parmTuple('opacity')[0].eval()
+    ior = shopshader.parm('ior').eval()
+    clearcoat = shopshader.parm('clearcoat').eval()
+    clearroug = shopshader.parm('clearcoatRoughness').eval()
+    occlusion = shopshader.parm('occlusion').eval()
+
     pbrShader.CreateInput('roughness', Sdf.ValueTypeNames.Float).Set(roughness)
     pbrShader.CreateInput('metallic', Sdf.ValueTypeNames.Float).Set(metallic)
     pbrShader.CreateInput('emissiveColor', Sdf.ValueTypeNames.Color3f).Set(eclr)
@@ -63,10 +87,12 @@ def newMaterialViaShop(shopshader, stage, path):
     pbrShader.CreateInput('ior', Sdf.ValueTypeNames.Float).Set(ior)
     pbrShader.CreateInput('clearcoat', Sdf.ValueTypeNames.Float).Set(clearcoat)
     pbrShader.CreateInput('clearcoatRoughness', Sdf.ValueTypeNames.Float).Set(clearroug)
-
+    pbrShader.CreateInput('occlusion', Sdf.ValueTypeNames.Float).Set(occlusion)
+    
     material.CreateSurfaceOutput().ConnectToSource(pbrShader, 'surface')
 
     return
+    
 def editMaterial(stage, matpath, dclr, roughness, metallic):
     
     #material = stage.GetPrimAtPath(matpath)
@@ -118,20 +144,14 @@ def write_preview_shading():
         for gpath in geoPaths:
             matName = gpath[1].rsplit('/')[-1]
             if matName is not '':
-                # using the shopmaterial path we try to find the first standard_surface we can
-                shopNodeShaders = hou.node(gpath[1]).children()
-                sshader = None
-                for shopShader in shopNodeShaders:
-                    if shopShader.type().name() == 'arnold::standard_surface':
-                        sshader = shopShader
-                        break
+                sshader = hou.node(gpath[1])
                 
                 if sshader:        
                     matPath = '/Materials/' + matName
                     if matPath not in pxrMatPaths:
                         pxrMatPaths.append(matPath)
                         #newMaterial(shopShader, stage, matPath, Gf.Vec3f(0.73938,0.742392,0.72749), 0.336918, 0)
-                        newMaterialViaShop(shopShader, stage, matPath)
+                        newMaterialViaShop(sshader, stage, matPath)
                         bindMaterial(stage, gpath[0], matPath)
                     else:
                         bindMaterial(stage, gpath[0], matPath)
@@ -142,5 +162,5 @@ def write_preview_shading():
     # the exported shaded usd path to sucessfully pull this together
     stage.Export(filename + '.usda')
     os.chdir(dirPath)
-    UsdUtils.CreateNewARKitUsdzPackage(fileNoPath+'.usda', fileNoPath + '.usdz') 
+    UsdUtils.CreateNewARKitUsdzPackage(fileNoPath+'.usda', fileNoPath + '.usdz')
         
