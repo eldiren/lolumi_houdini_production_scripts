630f6896e265fcef5143cf69e3d30874b55855e9
Eldiren <logicielumiere@gmail.com>
2020-08-02 04:12:57 -0400

    Figured out the detail flag issue, added Octane
    With the detail flag set now nodes will be collapsed on tool execution

--- usd_material_network.py
+++ usd_material_network.py
@@ -9,7 +9,7 @@ def check_type(type):
             
     return False 
     
-qpath = hou.ui.curDesktop().paneTabOfType(hou.paneTabType.TreeView).pwd().path()
+qpath = hou.ui.curDesktop().paneTabOfType(hou.paneTabType.NetworkEditor).pwd().path()
 
 cat = str( hou.node(qpath).childTypeCategory() )
 category = cat.split(' ')[-1].replace('>', '')
@@ -20,19 +20,14 @@ if category == 'Vop':
     collect = con.createNode('collect', 'newmat')
     
     if check_type('arnold_materialbuilder'):
-        sub_con = con.createNode('arnold_materialbuilder')
+        sub_con = con.createNode('arnold_material')
+        sub_con.setDetailLowFlag(True)
         sub_con.setGenericFlag(hou.nodeFlag.Material, False)
         collect.setNextInput(sub_con)
         
-        out = sub_con.children()[0]
-        r_mat = sub_con.createNode('arnold::standard_surface')
-        out.setInput( 0, r_mat, 0 )
-    
-        r_mat.setCurrent(True, clear_all_selected=True)
-        sub_con.layoutChildren()
-    
     if check_type('redshift_usd_material'):
         sub_con = con.createNode('redshift_usd_material')
+        sub_con.setDetailLowFlag(True)
         sub_con.setGenericFlag(hou.nodeFlag.Material, False)
         collect.setNextInput(sub_con)
     
@@ -44,14 +39,22 @@ if category == 'Vop':
     
     if check_type('pxrdisney'):
         sub_con = con.createNode('pxrdisney')
+        sub_con.setDetailLowFlag(True)
         sub_con.setGenericFlag(hou.nodeFlag.Material, False)
         collect.setNextInput(sub_con)
     
+    if check_type('octane::NT_MAT_UNIVERSAL'):
+        # Octane doesn't appear to support collect yet
+        sub_con = con.createNode('octane::NT_MAT_UNIVERSAL','newmat_oct')
+        sub_con.setDetailLowFlag(True)
+    
     sub_con = con.createNode('usdpreviewsurface')
+    sub_con.setDetailLowFlag(True)
     sub_con.setGenericFlag(hou.nodeFlag.Material, False)
     collect.setNextInput(sub_con)
     
     sub_con = con.createNode('principledshader::2.0')
+    sub_con.setDetailLowFlag(True)
     sub_con.setGenericFlag(hou.nodeFlag.Material, False)
     collect.setNextInput(sub_con)
     
