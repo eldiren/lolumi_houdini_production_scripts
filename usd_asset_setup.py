2362cf8c7d8ba2dcc72cba834eb2463a08ced69d
Eldiren <logicielumiere@gmail.com>
2020-10-26 19:52:48 -0400

    More fixes
    Fixed save path expressions, add graft for lights to avoid wierd overs in scene graph, asset location/extension changes, using scene camera import since Solaris cameras and weak on parameter controls, added octane check

--- usd_asset_setup.py
+++ usd_asset_setup.py
@@ -1,6 +1,6 @@
-# Place this as a python script in a shelf tool and if your in a Material Library this
-# Will create a Collect node with all the currently support hydra delegate shaders
-# attached, ready for shading
+# Place this as a python script in a shelf tool and if your in a LOP context
+# this will set up a basic asset structure (geom, shade, and asset layers),
+# plus a lookdev layer for quick inspection in USDView
 
 def check_type(type): 
     for ntype in hou.vopNodeTypeCategory().nodeTypes().keys(): 
@@ -28,8 +28,10 @@ if category == 'Lop':
     casset = con.createNode('configurelayer', 'configurelayer_asset')
     refasset = con.createNode('reference')
     reflights = con.createNode('reference', 'studio_light_set01')
+    lmixer = con.createNode('lightmixer')
+    lcoll = con.createNode('collection')
+    lgraft = con.createNode('graft', 'graft_lights')
     usdrop = con.createNode('usd_rop')
-    camera = con.createNode('camera')
     rsettings = con.createNode('rendersettings')
     beautyrvar = con.createNode('rendervar', 'beauty')
     albedorvar = con.createNode('rendervar', 'albedo')
@@ -43,6 +45,9 @@ if category == 'Lop':
     depthrvar = con.createNode('rendervar', 'depth')
     rproduct = con.createNode('renderproduct')
     usdrender = con.createNode('usdrender_rop')
+    objnet = con.createNode('objnet', 'cam_net')
+    camera = objnet.createNode('cam')
+    simport = con.createNode('sceneimport', 'import_cam')
     
     acsop.setFirstInput(filesop)
     outputsop.setFirstInput(acsop)
@@ -54,10 +59,13 @@ if category == 'Lop':
     mlib.setFirstInput(geom)
     shade.setFirstInput(mlib)
     casset.setFirstInput(shade)
-    refasset.setFirstInput(casset)
-    camera.setFirstInput(refasset)
-    reflights.setFirstInput(camera)
-    beautyrvar.setFirstInput(reflights)
+    refasset.setInput(1, casset)
+    simport.setFirstInput(refasset)
+    lgraft.setFirstInput(simport)
+    lcoll.setFirstInput(reflights)
+    lmixer.setFirstInput(lcoll)
+    lgraft.setNextInput(lmixer)
+    beautyrvar.setFirstInput(lgraft)
     albedorvar.setFirstInput(beautyrvar)
     diffdirvar.setFirstInput(albedorvar)
     diffidirvar.setFirstInput(diffdirvar)
@@ -75,27 +83,34 @@ if category == 'Lop':
     hou_parm_template = hou.StringParmTemplate("assetname", "Aseet Name", 1, default_value=(["asset"]), naming_scheme=hou.parmNamingScheme.Base1, string_type=hou.stringParmType.Regular, menu_items=([]), menu_labels=([]), icon_names=([]), menu_type=hou.menuType.StringReplace)
     confasset.addSpareParmTuple(hou_parm_template)
 
-    sopcreate.parm('enable_pathprefix').set(0)
+    hou_parm_template = hou.StringParmTemplate("savepath", "Save Path", 1, default_value=(["$HIP/usd/"]), naming_scheme=hou.parmNamingScheme.Base1, string_type=hou.stringParmType.FileReference, menu_items=([]), menu_labels=([]), icon_names=([]), item_generator_script="", item_generator_script_language=hou.scriptLanguage.Python, menu_type=hou.menuType.StringReplace)
+    confasset.addSpareParmTuple(hou_parm_template)
+
+    sopcreate.parm('pathprefix').set('/')
     acsop.parm('name1').set('name')
     acsop.parm('class1').set(1) #primitive
     acsop.parm('type1').set(3) #string
     acsop.parm('string1').set('`chs("../../../../configure_asset/assetname")`')
     geom.parm('setsavepath').set(1)
-    geom.parm('savepath').set('$HIP/usd/`chs("../configure_asset/assetname")`_geom.usdc')
+    geom.parm('savepath').set('`chs("../configure_asset/savepath")``chs("../configure_asset/assetname")`_geom.usdc')
     geom.parm('setdefaultprim').set(1)
     geom.parm('defaultprim').set('/`chs("../configure_asset/assetname")`')
+    geom.parm('startnewlayer').set(1)
     mlib.parm('matpathprefix').set('/`chs("../configure_asset/assetname")`/materials/')
     shade.parm('setsavepath').set(1)
-    shade.parm('savepath').set('$HIP/usd/`chs("../configure_asset/assetname")`_shade.usdc')
+    shade.parm('savepath').set('`chs("../configure_asset/savepath")``chs("../configure_asset/assetname")`_shade.usdc')
+    shade.parm('startnewlayer').set(1)
     casset.parm('setsavepath').set(1)
     casset.parm('setdefaultprim').set(1)
     casset.parm('defaultprim').set('/`chs("../configure_asset/assetname")`')
-    casset.parm('savepath').set('$HIP/usd/`chs("../configure_asset/assetname")`.usda')
-    #refasset.parm('reftype').set('prim')
+    casset.parm('savepath').set('`chs("../configure_asset/savepath")``chs("../configure_asset/assetname")`.usda')
     refasset.parm('reftype').set('inputref')
     refasset.parm('primpath').set('/lookdev/`chs("../configure_asset/assetname")`')
-    reflights.parm('primpath').set('/lookdev/$OS')
-    reflights.parm('filepath1').set('$ASSETS_DIR/studios/light_rigs/usd/studio_light_set01.usd')
+    reflights.parm('primpath').set('/$OS')
+    reflights.parm('filepath1').set('$ASSETS_DIR/studios/light_rigs/usd/studio_light_set01.usdc')
+    lcoll.parm('primpattern1').set('/studio_light_set01/*')
+    lcoll.parm('collectionname').set('set01')
+    lgraft.parm('destpath').set('/lookdev')
     beautyrvar.parm('primpath').set('/lookdev/Render/Products/Vars/$OS')
     beautyrvar.parm('sourceName').set('RGBA')
     beautyrvar.parm('dataType').set('color3f')
@@ -147,12 +162,13 @@ if category == 'Lop':
     depthrvar.parm('xn__driverparametersaovname_jebkd').set('Z')
     depthrvar.parm('xn__driverparametersaovformat_shbkd').set('float3')
 
-    camera.parm('primpath').set('/lookdev/cameras/$OS')
-    camera.parm('horizontalAperture').set(16)
-    camera.parm('verticalAperture').set(9)
+    simport.parm('objdestpath').set('/lookdev/cameras')
+    simport.parm('rootobject').set(objnet.path())
+    simport.parm('filter').set('!!OBJ/CAMERA!!')
+    simport.parm('objects').set('*')
     usdrop.parm('enableoutputprocessor_simplerelativepaths').set(0)
     usdrop.parm('defaultprim').set('/lookdev')
-    usdrop.parm('lopoutput').set('$HIP/usd/`chs("../configure_asset/assetname")`_lookdev.usda')
+    usdrop.parm('lopoutput').set('`chs("../configure_asset/savepath")``chs("../configure_asset/assetname")`_lookdev.usda')
     rproduct.parm('orderedVars').set(beautyrvar.parm('primpath').eval() + ' ' + 
     albedorvar.parm('primpath').eval() + ' ' +
     diffdirvar.parm('primpath').eval() + ' ' +
@@ -168,7 +184,7 @@ if category == 'Lop':
     rproduct.parmTuple('resolution')[0].set(1920)
     rproduct.parmTuple('resolution')[1].set(1080)
     rsettings.parm('primpath').set('/lookdev/Render/$OS')
-    rsettings.parm('camera').set('/lookdev/cameras/camera1')
+    rsettings.parm('camera').set('/lookdev/cameras/'+ camera.name())
     rsettings.parmTuple('resolution')[0].set(1920)
     rsettings.parmTuple('resolution')[1].set(1080)
     rsettings.setDisplayFlag(True)
@@ -182,5 +198,8 @@ if category == 'Lop':
         
     if check_type('pxrdisney'):
         pass
-        
+    
+    if check_type('octane::NT_MAT_UNIVERSAL'):
+        pass
+
     con.layoutChildren()
